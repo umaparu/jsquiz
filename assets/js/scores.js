c9170f280ce6f62c98eb53427abe94f6012b5936
umaparu <umaambikesan@yahoo.com>
2023-02-08 13:06:01 +0000

    Added comments to score.js

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,16 +1,21 @@
 
+// Variable for storing high scores
   var highscoresList = document.querySelector('#highscores');
 
+  // Get refernce for clear button
   var clearBtn = document.querySelector('#clear');
   // Add event listener to start button
   clearBtn.addEventListener('click', clearScore);
 
+// SHow hig scores when page is loaded
 showScores();
 
+// Function to show high scores
 function showScores() {
 
   highscoresList.className="button";
 
+  // Get  high scores from local storage
   var highScore = localStorage.getItem("highscore");
   var myScore = localStorage.getItem("myscore");
 
@@ -21,6 +26,7 @@ function showScores() {
   var hArr;
   var hScore;
   
+  // High score is kept in the format "INITIALS-SCORE". Split and parse the score part
   if (myScore != null) {
      mArr = myScore.split('-');
      mScore= parseInt(mArr[1]);
@@ -32,7 +38,8 @@ function showScores() {
      hScore= parseInt(hArr[1]);
 
   }
-
+  
+  // Compare high score from local storage and current score. If new score is higher store that as high score
   if ((highScore != null)  &&  (myScore != null))
   {
     if (mScore > hScore) {
@@ -64,6 +71,7 @@ function showScores() {
   highscoresList.innerHTML = listText;
 }
 
+// Clear highscores stored in local storage
 function clearScore(){
   highscoresList.innerHTML = "<ol></ol>";
   localStorage.removeItem("highscore");
